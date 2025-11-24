bfad812c05683766f550f0558cc27021acb7d97e
Jared Bieker <jbieker@eltoncyber.com>
2025-11-24 21:02:58 +0000

    add navbar

---
+++ app/mylist/page.jsx
@@ -0,0 +1,11 @@
+import GridComponent from "../../components/GridComponent";
+import NavBar from "../../components/NavBar";
+
+export default function Home() {
+  return (
+    <div >
+     <NavBar /> 
+      <GridComponent />
+    </div>
+  );
+}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,58 @@
 "use client";
 
-const NavBar = () => {
-return (
-"test"
-)
-}
-export default NavBar;
+import Link from "next/link";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { Url } from "next/dist/shared/lib/router/router";
+
+interface INavbarProps {}
+
+const navItems = [
+  {
+    id: "mylist",
+    label: "mylist",
+    href: "/mylist",
+  },
+  {
+    id: "otherlists",
+    label: "otherlists",
+    href: "/otherlists",
+  },
+  {
+    id: "login",
+    label: "login",
+    href: "/login",
+  },
+];
+
+const Navbar: React.FunctionComponent<INavbarProps> = (props) => {
+  const pathname = usePathname();
+  const isActive = (path: Url) => pathname === path;
+
+  return (
+    <nav className="pb-4 md:pb-8 flex justify-between items-center">
+      <Link
+        href="/"
+        className="text-lg md:text-3xl font-bold text-spotify-green"
+      >
+        webapp
+      </Link>
+     <ul className="flex justify-end items-center gap-4">
+        {navItems.map((eachItem) => (
+          <li key={eachItem.id}>
+            <Link
+              href={eachItem.href}
+              className={`${
+                isActive(eachItem.href) ? "text-spotify-green" : ""
+              }`}
+            >
+              {eachItem.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
