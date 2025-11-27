4e85032d44f868c20696d422dbb52fdfeeb396a7
Jared Bieker <jbieker@eltoncyber.com>
2025-11-27 06:15:02 +0000

    login/logout

--- components/LoginComponent.tsx
+++ components/LoginComponent.tsx
@@ -1,10 +1,28 @@
 "use client";
-import { signIn } from 'next-auth/react';
+import { getAuth, getRedirectResult, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signInWithRedirect, signOut } from "firebase/auth";
+import {auth} from "../services/FirebaseService";
 
-export default function LoginPage() {
+export default function Home() {
+  const provider = new GoogleAuthProvider();
+  
   return (
-    <button onClick={() => signIn('google')}>
-      Sign in with Google
-    </button>
+    <div>
+     <button onClick={() => signInWithPopup(auth, provider)}>Sign in with Google</button>
+     <button onClick={() => signOut(auth)}>Sign out</button>
+    </div>
   );
-}   
+}
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    console.log(uid);
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    console.log("signed out");
+  }
+});
