00fb2612b54f71d5d551b0fb39a61321714de5aa
Jared Bieker <jbieker@eltoncyber.com>
2025-11-29 07:35:52 +0000

    rm authbutton references

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,5 @@
 "use client";
 import LoginPage from "../components/LoginComponent";
-import AuthButton from "../components/AuthButton";
-import NavBar from "../components/NavBar";
-import Navbar from "../components/NavBar";
 
 
 export default function Home() {
